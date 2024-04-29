b7e6354c3885e3e170caae182bcdc9e3be2db0cb
Bunnyf1uff <timodevops@gmail.com>
2024-04-29 18:30:45 +0700

    Mengatur transisi dengan javascript Hooks Vue

--- Project Vue/app.js
+++ Project Vue/app.js
@@ -25,6 +25,21 @@ var app = new Vue({
     methods: {
         addItem: function(product) {
             this.cart.push(product)
+        },
+        before: function(el) {
+            el.className = 'd-none'
+        },
+        enter: function(el) {
+            var delay = el.dataset.index * 100
+            setTimeout(function () {
+                el.className = "row d-flex mb-3 align-items-center animate__animated animate__fadeInRight"
+            }, delay);
+        },
+        leave: function(el) {
+            var delay = el.dataset.index * 100
+            setTimeout(function () {
+                el.className = "row d-flex mb-3 align-items-center animate__animated animate__fadeOutRight"
+            }, delay);
         }
     }
 })
