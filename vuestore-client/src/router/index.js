e52868f7f3df578ebb0f0478f91aef252522d667
Bunnyf1uff <timodevops@gmail.com>
2024-07-31 14:28:23 +0700

    Setup project Vuestore

--- vuestore-client/src/router/index.js
+++ vuestore-client/src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Product from '../views/product/product-index.vue'
+import ProductDetail from '../views/product/product-detail.vue'
+import Cart from '../views/cart/cart-index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'product',
+    component: Product
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/product/:id',
+    name: 'product-detail',
+    component: ProductDetail
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+  },
 ]
 
 const router = new VueRouter({
---
+++ vuestore-client/src/views/product/product-index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Product Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
