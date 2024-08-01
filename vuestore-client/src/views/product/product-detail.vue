afce5af9be86cf606530c4f79c24f65fa5554213
Bunnyf1uff <timodevops@gmail.com>
2024-08-02 01:20:53 +0700

    Menampilkan data product di shopping cart dan menambahkan navbar

--- vuestore-client/src/views/product/product-detail.vue
+++ vuestore-client/src/views/product/product-detail.vue
@@ -1,11 +1,38 @@
 <template>
     <div>
-      <h1>Detail Product</h1>
+      <div id="page-wrap">
+        <div id="img-wrap">
+          <img :src="product.imageUrl" alt="">
+        </div>
+        <div id="product-details">
+          <h1>{{ product.name }}</h1>
+          <h3 id="price">{{ product.price }}</h3>
+          <p>Average Rating: {{ product.averageRating }}</p>
+          <button id="add-to-cart">Add to Cart</button>
+          <p>{{ product.description }}</p>
+        </div>
+      </div>
     </div>
 </template>
   
 <script>
+import { products } from '../../data-seed'; 
 export default {
+  data() {
+    return {
+      products
+    }
+  },
+  computed: {
+    product() {
+      return this.products.find(p => {
+        return p.id === this.$route.params.id
+      })
+    }
+  }, 
+  mounted() {
+    console.log(this.product);
+  }
   
 };
 </script>  
