39bdf9cc19d0c8c46dfc406bb057a3e748cccc15
Bunnyf1uff <timodevops@gmail.com>
2024-08-03 01:46:27 +0700

    membuat reusable components dan props

--- vuestore-client/src/views/cart/cart-index.vue
+++ vuestore-client/src/views/cart/cart-index.vue
@@ -2,18 +2,7 @@
   <div>
     <div id="page-wrap">
       <h1>Shopping Cart</h1>
-      <div 
-      v-for="item in cartItems"
-      :key="item.id"
-      class="product-container"
-      >
-        <img :src="item.imageUrl" alt="" class="product-image">
-        <div class="details-wrap">
-          <h3>{{ item.name }}</h3>
-          <p>{{ item.price }}</p>
-        </div>
-        <button class="remove-button">Remove</button>
-      </div>
+      <ItemCart v-for="item in cartItems" :key="item.id" :item="item"></ItemCart>
       <h3 id="total-price">Total: {{ totalPrice }}</h3>
       <button id="checkout-button">Checkout</button>
     </div>
@@ -22,7 +11,12 @@
 
 <script>
 import { cartItems } from '../../data-seed';
+import ItemCart from '../../components/ItemCart.vue';
+
 export default {
+  components: {
+    ItemCart
+  },
   data() {
     return {
       cartItems
@@ -49,25 +43,5 @@ export default {
   }
   #checkout-button {
     width: 100%;
-  }
-  .product-container {
-    align-content: 'center';
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    padding: 16px;
-    width: 100%;
-  }
-  .product-image {
-    flex: 1;
-    height: 100px;
-    max-width: 100px;
-  }
-  .details-wrap {
-    padding: 0 16px;
-    flex: 3;
-  }
-  .remove-button {
-    flex: 1;
-    margin: auto;
   }
 </style>
