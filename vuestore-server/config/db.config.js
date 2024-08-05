a1db42afed77a30b51ae1648b8e1801bf9d9faff
Bunnyf1uff <timodevops@gmail.com>
2024-08-05 17:03:16 +0700

    tweaking database

---
+++ vuestore-server/config/db.config.js
@@ -0,0 +1,5 @@
+const password = require('../config/password');
+
+module.exports = {
+    url: `mongodb+srv://2003974:${password}@cluster1.nwldfvp.mongodb.net/vuestore?retryWrites=true&w=majority&appName=Cluster1`
+}
--- vuestore-server/config/dbconfig.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: "mongodb://localhost:27017/collect_vuestore"
-}
